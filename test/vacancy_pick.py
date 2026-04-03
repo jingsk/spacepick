8943948b3d476032fc12b0e0160dd192389a65d3
jingsk <jingsk129@gmail.com>
2026-04-03 12:25:15 -0700

    read in structure and export images of vacancies in 3 modes.

---
+++ test/vacancy_pick.py
@@ -0,0 +1,40 @@
+from ase.io import read, write
+from ase.visualize.plot import plot_atoms
+import numpy as np
+import matplotlib.pyplot as plt
+
+atoms = read('MoS2_ortho.vasp') * [2,4,1]
+atoms_S_mapping = np.array([atom.index for atom in atoms if atom.scaled_position[2]>0.5])
+atoms_S = atoms[atoms_S_mapping]
+
+from vacancies import Selector
+vec_dis_mat = atoms_S.get_all_distances(mic=True, vector=True)
+m = 9
+selector = Selector(vec_dis_mat)
+
+fig, axs = plt.subplots(3,3, figsize = [3*3,2.25*3], layout='constrained')
+
+for i in range(m):
+    atoms_copy = atoms.copy()
+    to_del = selector.get_indices(n_subset=i+1, mode='dispersed')
+    del atoms_copy[atoms_S_mapping[to_del]]
+    plot_atoms(atoms_copy, ax=axs.ravel()[i])
+fig.savefig('dispersed.png', dpi=300)
+
+
+fig, axs = plt.subplots(3,3, figsize = [3*3,2.25*3], layout='constrained')
+for i in range(m):
+    atoms_copy = atoms.copy()
+    to_del = selector.get_indices(n_subset=i+1, mode='clustered')
+    del atoms_copy[atoms_S_mapping[to_del]]
+    plot_atoms(atoms_copy, ax=axs.ravel()[i])
+fig.savefig('clustered.png', dpi=300)
+
+fig, axs = plt.subplots(3,3, figsize = [3*3,2.25*3], layout='constrained')
+for i in range(m):
+    atoms_copy = atoms.copy()
+    to_del = selector.get_indices(n_subset=i+1, mode='linear', direction_pref='yx', tol=[1e-3, 10])
+    del atoms_copy[atoms_S_mapping[to_del]]
+    plot_atoms(atoms_copy, ax=axs.ravel()[i])
+fig.savefig('linear.png', dpi=300)
+
